872411db6fd5e3f298aaa7667f88880b8992f23e
sarveshamberkar <sarvesh.amberkar@gmail.com>
2021-05-12 22:26:52 +0530

    Assignment 21 completed

--- flask/practice_corey/flaskblog.py
+++ flask/practice_corey/flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for,flash
 from form import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -33,6 +33,8 @@ def about():
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
     return render_template('register.html', title='Register', form=form)
 
 
